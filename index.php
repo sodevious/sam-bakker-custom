38124b18a133d6ad85d618060da7eadef6c5ec37
Nicole Dominguez <sodevious.net@gmail.com>
2013-10-06 21:59:59 -0400

    sales

--- index.php
+++ index.php
@@ -20,10 +20,84 @@ get_header(); ?>
 		<div class="clearfix"></div>
 	</div>
 	
-	<div class="clearfix"></div>
+
+	<div class="index-content twelve columns alpha">
+		
+		<p>It’s my belief that millions of business owners never realize their businesses full potential, further themselves, grow and use the power that online marketing has to reach their ultimate vision for their company. It’s my intention to show business owners all around the world that they can not only automate their sales by taking their business online, but extend their reach and connection with customers.</p>
+
+		<p>Here you will find <strong>tangible actionable information</strong> that will help you get started online, not only generating a solid ROI for your business but creating and implementing systems that allow you to unplug from your business to enjoy life without the burden of having to work day in and day out.</p>
+
+		<h3>The Best Way To Automate Your Business And Make More Sales Is Marketing Online!</h3>
+
+		<p>There are many benefits to online marketing. One of the biggest that has helped me and my clients is that online marketing gives us the ability to understand who our customer is more than any other platform. We know if they are single, their age, where they shop, what sites they hangout at and that’s only scratching the surface. It’s created a opportunity for smaller businesses to grow quickly.</p>
+
+
+		<blockquote>"You can be a David vs. a Goliath, if you get it right." Richard Branson – Virgin</blockquote>
+
+		<p>Richard Branson said this when he was referring to Virgin in its earlier years where it fought many other bigger competitors and succeeded because he knew who his customers were better than anyone. The same can be true for you online. You have the opportunity to find more out about your customers than you ever had before meaning you can create an unfair advantage for your business, even if it’s small.</p>
+
+		<p>After years of ‘in field’ experience, training hundreds of thousands of business owners from around the world, consulting to some of the biggest companies in New Zealand, selling over a million dollars worth of product in my own business online by the age of 24 and working with the best in the personal development and business industry names like “Jeff Slayter, Kane Minkus, Bill Walsh, Roger Hamilton, David Bullock, Mark Anastasi and many others, I have developed a number of unique methods for achieving better success in your business and freedom with the use of a variety of important but easy to understand online marketing tools and techniques.</p>
+
+		<p>I want to give you a gift today allowing you access on this website and the ability to learn my best methods for successfully transitioning your business online. I have divided the process up into 3 easy to follow steps. Learning the ropes, Setup and Sales In this short 30 minute presentation I am going to show you all that you need to develop a winning online marketing plan for your business within the next 30 days . If you would like to get access to this presentation all you need to do is click here, submit your name and email and you’ll be sent directly to the presentation.</p>
+
+		<p>Thank you for visiting my website here, subscribing to my information or becoming a customer - I look forward to showing you what I have learnt online in the hopes that it gives you the tools you need to setup your business online generating sales and business automatically while freeing up your time to enjoy life the way you want to. </p>
+
+		<p>To your Success,</p>
+		<img src="<?php echo get_template_directory_uri(); ?>/assets/sam-sig.png" />
+	</div>
+
+
+	<div class="index-sidebar four columns omega" role="complementary">
+
 
 	<div class="testimonial_container">
 		<h2>What The Experts Are Saying</h2>
+			<div class="tile">
+				<a href="/testimonials">
+					<div class="overlay_tile_top">
+						"Keep up with the newest trends"
+					</div>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/jason_westland.jpg" />
+					<div class="overlay_tile_bottom">
+						<b>Jason Westland</b> - CEO & Founder of ProjectManager.com
+					</div>
+				</a>
+			</div>
+
+			<div class="tile">
+				<a href="/testimonials">
+					<div class="overlay_tile_top">
+						"Make more sales for your business online"
+					</div>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/bill_walsh.jpg" />
+					<div class="overlay_tile_bottom">
+						<b>Bill Walsh</b> - America's Business Expert
+					</div>
+				</a>
+			</div>
+			<div class="tile">
+				<a href="/testimonials">
+					<div class="overlay_tile_top">
+						"Build a massive list"
+					</div>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/david_bullock.jpg" />
+					<div class="overlay_tile_bottom">
+						<b>David Bullock</b> - CEO and Co-Founder of CEO Mastery
+					</div>
+				</a>
+			</div>
+
+			<div class="tile">
+				<a href="/testimonials">
+					<div class="overlay_tile_top">
+						"I recommend this to anyone"
+					</div>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/joel_peterson.jpg" />
+					<div class="overlay_tile_bottom">
+						<b>Joel Peterson</b> - Author, Speaker & Online Marketer
+					</div>
+				</a>
+			</div>
 			<div class="tile">
 				<a href="/testimonials">
 					<div class="overlay_tile_top">
@@ -38,57 +112,27 @@ get_header(); ?>
 
 			<div class="tile">
 				<a href="/testimonials">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/david_bullock.jpg" />
 					<div class="overlay_tile_top">
-						"Build a massive list"
+						"You can't go wrong investing with Sam"
 					</div>
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/mark_anastasi.jpg" />
 					<div class="overlay_tile_bottom">
-						<b>David Bullock</b> - CEO and Co-Founder of CEO Mastery
+						<b>Mark Anastasi</b> - NYTimes Best Seller & Author of The Laptop Millionaire
 					</div>
 				</a>
 			</div>
 
-			<div class="tile">
-				<a href="/testimonials">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/bill_walsh.jpg" />
-					<div class="overlay_tile_top">
-						"Make more sales for your business online"
-					</div>
-					<div class="overlay_tile_bottom">
-						<b>Bill Walsh</b> - America's Business Expert
-					</div>
-				</a>
-			</div>
 
-			<div class="tile">
-				<a href="/testimonials">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/joel_peterson.jpg" />
-					<div class="overlay_tile_top">
-						"I recommend this to anyone"
-					</div>
-					<div class="overlay_tile_bottom">
-						<b>Joel Peterson</b> - Author, Speaker & Online Marketer
-					</div>
-				</a>
-			</div>
 
-			<div class="tile">
-				<a href="/testimonials">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/austin_walsh.jpg" />
-					<div class="overlay_tile_top">
-						"Keep up with the newest trends"
-					</div>
-					<div class="overlay_tile_bottom">
-						<b>Jason Westland</b> - CEO & Founder
-					</div>
-				</a>
+</div>
 
-		</div>
-	</div>
+</div>
 
+
+	
 	<div class="clearfix"></div>
 
-	<div class="posts_index">
+<!--
 
 	<div class="post_container">
 		<h2>Recent Articles</h2>
@@ -106,7 +150,9 @@ get_header(); ?>
 		</div>
 	</div>
 
-	</div>
+-->
+
+
 
 	<div class="clearfix"></div>
 
@@ -124,5 +170,21 @@ get_header(); ?>
 		<div style="clear: both; display: block;">&nbsp;</div>
 	</div>
 
+
+	<div class="post_container">
+		<h2>Featured Articles</h2>
+		<?php rewind_posts(); ?>
+		<?php $my_query = new WP_Query('category_name=featured&posts_per_page=5'); ?>
+		<?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		<div class="tile">
+			<a href="<?php the_permalink() ?>">
+				<?php the_post_thumbnail('homepage-thumb'); ?>
+				<div class="overlay_tile">
+					<?php the_title() ?>
+				</div>
+			</a>
+		</div>
+		<?php endwhile; ?>
+	</div>
 
 <?php get_footer(); ?>
